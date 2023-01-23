3aaf5305129ecbc8f0b19a4fad12ab47f40e2699
PortalRomOS <portalromOS@gmail.com>
2023-01-23 14:48:06 +0000

    Change all to portalrom

---
+++ src/org/portalrom/setupwizard/PortalRomSettingsActivity.java
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2023 The PortalRom Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.portalrom.setupwizard;
+
+import static org.portalrom.setupwizard.SetupWizardApp.DISABLE_NAV_KEYS;
+import static org.portalrom.setupwizard.SetupWizardApp.KEY_SEND_METRICS;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.UserHandle;
+import android.text.Spannable;
+import android.text.SpannableString;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
+import android.text.style.ClickableSpan;
+import android.util.Log;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import com.google.android.setupcompat.util.WizardManagerHelper;
+
+import portalrom.hardware.PortalRomHardwareManager;
+import portalrom.providers.PortalRomSettings;
+
+public class PortalRomSettingsActivity extends BaseSetupWizardActivity {
+
+    public static final String TAG = PortalRomSettingsActivity.class.getSimpleName();
+
+    public static final String PRIVACY_POLICY_URI = "";
+
+    private SetupWizardApp mSetupWizardApp;
+
+    private CheckBox mMetrics;
+    private CheckBox mNavKeys;
+
+    private boolean mSupportsKeyDisabler = false;
+
+    private final View.OnClickListener mMetricsClickListener = view -> {
+        boolean checked = !mMetrics.isChecked();
+        mMetrics.setChecked(checked);
+        mSetupWizardApp.getSettingsBundle().putBoolean(KEY_SEND_METRICS, checked);
+    };
+
+    private final View.OnClickListener mNavKeysClickListener = view -> {
+        boolean checked = !mNavKeys.isChecked();
+        mNavKeys.setChecked(checked);
+        mSetupWizardApp.getSettingsBundle().putBoolean(DISABLE_NAV_KEYS, checked);
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mSetupWizardApp = (SetupWizardApp) getApplication();
+        setNextText(R.string.next);
+
+        String os_name = getString(R.string.os_name);
+        String privacyPolicy = getString(R.string.services_pp_explanation, os_name);
+        String policySummary = getString(R.string.services_find_privacy_policy, PRIVACY_POLICY_URI);
+        String servicesFullDescription = getString(R.string.services_full_description,
+                privacyPolicy, policySummary);
+        getGlifLayout().setDescriptionText(servicesFullDescription);
+
+        View metricsRow = findViewById(R.id.metrics);
+        metricsRow.setOnClickListener(mMetricsClickListener);
+        metricsRow.requestFocus();
+        String metricsHelpImprovePortalRom =
+                getString(R.string.services_help_improve_cm, os_name);
+        String metricsSummary = getString(R.string.services_metrics_label,
+                metricsHelpImprovePortalRom, os_name, os_name);
+        final SpannableStringBuilder metricsSpan = new SpannableStringBuilder(metricsSummary);
+        metricsSpan.setSpan(new android.text.style.StyleSpan(android.graphics.Typeface.BOLD),
+                0, metricsHelpImprovePortalRom.length(), Spannable.SPAN_EXCLUSIVE_EXCLUSIVE);
+        TextView metrics = (TextView) findViewById(R.id.enable_metrics_summary);
+        metrics.setText(metricsSpan);
+        mMetrics = (CheckBox) findViewById(R.id.enable_metrics_checkbox);
+
+        View navKeysRow = findViewById(R.id.nav_keys);
+        navKeysRow.setOnClickListener(mNavKeysClickListener);
+        mNavKeys = (CheckBox) findViewById(R.id.nav_keys_checkbox);
+        mSupportsKeyDisabler = isKeyDisablerSupported(this);
+        if (mSupportsKeyDisabler) {
+            mNavKeys.setChecked(PortalRomSettings.System.getIntForUser(getContentResolver(),
+                    PortalRomSettings.System.FORCE_SHOW_NAVBAR, 0, UserHandle.USER_CURRENT) != 0);
+        } else {
+            navKeysRow.setVisibility(View.GONE);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        updateDisableNavkeysOption();
+        updateMetricsOption();
+    }
+
+    @Override
+    protected void onNextPressed() {
+        Intent intent = WizardManagerHelper.getNextIntent(getIntent(), Activity.RESULT_OK);
+        nextAction(NEXT_REQUEST, intent);
+    }
+
+    @Override
+    protected int getLayoutResId() {
+        return R.layout.setup_portalrom_settings;
+    }
+
+    @Override
+    protected int getTitleResId() {
+        return R.string.setup_services;
+    }
+
+    @Override
+    protected int getIconResId() {
+        return R.drawable.ic_features;
+    }
+
+    private void updateMetricsOption() {
+        final Bundle myPageBundle = mSetupWizardApp.getSettingsBundle();
+        boolean metricsChecked =
+                !myPageBundle.containsKey(KEY_SEND_METRICS) || myPageBundle
+                        .getBoolean(KEY_SEND_METRICS);
+        mMetrics.setChecked(metricsChecked);
+        myPageBundle.putBoolean(KEY_SEND_METRICS, metricsChecked);
+    }
+
+    private void updateDisableNavkeysOption() {
+        if (mSupportsKeyDisabler) {
+            final Bundle myPageBundle = mSetupWizardApp.getSettingsBundle();
+            boolean enabled = PortalRomSettings.System.getIntForUser(getContentResolver(),
+                    PortalRomSettings.System.FORCE_SHOW_NAVBAR, 0, UserHandle.USER_CURRENT) != 0;
+            boolean checked = myPageBundle.containsKey(DISABLE_NAV_KEYS) ?
+                    myPageBundle.getBoolean(DISABLE_NAV_KEYS) :
+                    enabled;
+            mNavKeys.setChecked(checked);
+            myPageBundle.putBoolean(DISABLE_NAV_KEYS, checked);
+        }
+    }
+
+    private static boolean isKeyDisablerSupported(Context context) {
+        final PortalRomHardwareManager hardware = PortalRomHardwareManager.getInstance(context);
+        return hardware.isSupported(PortalRomHardwareManager.FEATURE_KEY_DISABLE);
+    }
+}
--- tests/Android.mk
+++ tests/Android.mk
@@ -11,19 +11,19 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 # Include all test java files.
 LOCAL_SRC_FILES := $(call all-java-files-under, src)
 
-LOCAL_PACKAGE_NAME := LineageSetupWizardTests
+LOCAL_PACKAGE_NAME := PortalRomSetupWizardTests
 LOCAL_CERTIFICATE := platform
 
 LOCAL_PRIVATE_PLATFORM_APIS := true
 
-LOCAL_REQUIRED_MODULES := privapp_whitelist_org.lineageos.setupwizard-tests.xml
+LOCAL_REQUIRED_MODULES := privapp_whitelist_org.portalrom.setupwizard-tests.xml
 
 LOCAL_USES_LIBRARIES := android.test.runner
 
 include $(BUILD_PACKAGE)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := privapp_whitelist_org.lineageos.setupwizard-tests.xml
+LOCAL_MODULE := privapp_whitelist_org.portalrom.setupwizard-tests.xml
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_PATH := $(TARGET_OUT_ETC)/permissions
